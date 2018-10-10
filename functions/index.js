7f7a1e550d9bdeb94c487a8d003ee7d76e1f6a3c
Hideaki Masuda <masuda@leapmind.io>
2018-10-10 12:33:04 +0900

    update password

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const basicAuth = require('basic-auth-connect')
 
 const app = express()
 
-app.use(basicAuth('admin', 'blueoil'))
+app.use(basicAuth('admin', 'lm-blueoil#1234'))
 
 app.get('/auth', (req, res) => {
   res.redirect('/HzrgN3KTNqBuYYfWPN9rEAWGMRY09V8rzBX0dOyeEQr7zH83hEYBa0FdYdjQJ3Ud/index.html')
